25b9ac744d592ffb08ff60160925f8ff99cfe576
Diana <dianagithubdavis@gmail.com>
2022-03-25 18:19:50 -0400

    Moves pwLength variable inside the addNewPassword function

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const charArray = charString.split('');
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-const pwLength = getRandomNumber(10, 18);  
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
 const addNewPassword = () => {
     //console.log('password length is     ' + pwLength);
-    let result = [];
+    const pwLength = getRandomNumber(10, 18);  
+    const result = [];
     for (let i = 0; i < pwLength; i ++) {
         result.push(charArray[getRandomNumber(0, charArray.length + 1)]);
     }
-    return result.join('') + `      ${result.length} digits`;
+    return `${result.join('')} ${result.length} digits`;
 }
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
