872d060060f837f3a585c9a78f7254b7c3b8b717
Diana <dianagithubdavis@gmail.com>
2022-03-25 08:16:00 -0400

    adds JS

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // Start your code ***HERE*** =========
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+const charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+-={}[]|\:;<>?,./";
+const charArray = charString.split('');
 
 // create a global variable called "pwLength" with a number between 10 and 18
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+const pwLength = getRandomNumber(10, 18);  
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+const addNewPassword = () => {
+    //console.log('password length is     ' + pwLength);
+    let result = [];
+    for (let i = 0; i < pwLength; i ++) {
+        result.push(charArray[getRandomNumber(0, charArray.length + 1)]);
+    }
+    return result.join('') + `      ${result.length} digits`;
+}
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
@@ -16,3 +30,5 @@ let buttonHandler = () => {
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+
+//console.log(addNewPassword());
