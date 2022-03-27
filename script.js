dce94c99f917d8725294d90d6417d88d4a081da0
Diana <dianagithubdavis@gmail.com>
2022-03-27 19:00:38 -0400

    playing with CSS

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ function getRandomNumber(min, max) {
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
 const addNewPassword = () => {
     //console.log('password length is     ' + pwLength);
-    const pwLength = getRandomNumber(10, 18);  
+    //Adds 1 to max because Math.floor; without adding 1 it will never choose 18
+    const pwLength = getRandomNumber(10, 19);  
     const result = [];
     for (let i = 0; i < pwLength; i ++) {
         result.push(charArray[getRandomNumber(0, charArray.length + 1)]);
     }
-    return `${result.join('')} ${result.length} digits`;
+    return result.join('');
 }
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
@@ -30,5 +31,4 @@ let buttonHandler = () => {
 genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
-
 //console.log(addNewPassword());
